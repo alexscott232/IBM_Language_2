c58cb43230f50672bb65a33f5e54b4c17675f207
George Chen <ghchen10@outlook.com>
2021-05-31 23:52:39 +0100

    landing

--- IBM_Language_2_project/Assets/Scripts/MainMenu.cs
+++ IBM_Language_2_project/Assets/Scripts/MainMenu.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void PlayGame()
+    public void Airport()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Hotel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 }
